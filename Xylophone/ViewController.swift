60fe3c722cde9baba2d9d6b66d78931be2f3b828
pixus <pix_us@mail.ru>
2021-05-18 22:39:22 -0400

    final

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,29 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+    }
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+        
     }
 
-
-    
-
+    @IBAction func pressButton(_ sender: UIButton) {
+        playSound (soundName: sender.currentTitle!)
+        print(sender.currentTitle!)
+        sender.alpha = 0.1
+    //   sender.backgroundColor = .gray
+        sender.alpha = 1 
+    }
 }
 
